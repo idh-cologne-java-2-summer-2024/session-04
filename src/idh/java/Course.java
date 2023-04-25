3b7a4fd56327f192d06ed678974bc77263be0779
Nils Reiter <nils.reiter@uni-koeln.de>
2023-04-25 13:05:09 +0200

    Updated course name

--- src/idh/java/Course.java
+++ src/idh/java/Course.java
@@ -36,7 +36,7 @@ public class Course {
 	}
 
 	public static void main(String[] args) throws Exception {
-		Course java2 = new Course("Softwaretechnologie: Java 2", 5);
+		Course java2 = new Course("Fortgeschrittene Programmierung (Java 2)", 5);
 		java2.addMember("Liraz Lucinde");
 		java2.addMember("Floro Mehrnaz");
 		java2.addMember("Emmanuelle Dinesh"); 
