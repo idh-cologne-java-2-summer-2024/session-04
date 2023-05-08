1418495c7a66a1c98172179c4275541bb0ea4ce9
Nils Reiter <nils.reiter@uni-koeln.de>
2023-05-08 14:50:19 +0200

    added code from class

--- src/idh/java/Course.java
+++ src/idh/java/Course.java
@@ -1,6 +1,8 @@
 package idh.java;
 
-public class Course {
+import java.util.Iterator;
+
+public class Course implements Iterable<Student> {
 	Student[] members;
 	
 	int nextPosition = 0;
@@ -57,5 +59,33 @@ public class Course {
 		java2.addMember("Isaac Sumire");
 		System.out.println(java2.getDescription());
 		
+		StudentIterator iter = new StudentIterator(java2);
+		
+		while(iter.hasNext()) {
+			System.out.println(iter.next().getName());
+		}
+		System.out.println();
+		
+		iter = new StudentIterator(java2);
+		while(iter.hasNext()) {
+			System.out.println(iter.next().getName());
+		}
+		System.out.println();
+		
+		
+		for (Student student : java2) {
+			System.out.println(student.getName());
+			
+			for (Student student2 : java2) {
+				System.out.println(student2.getName());
+			}
+
+		}
+		
+	}
+
+	@Override
+	public Iterator<Student> iterator() {
+		return new StudentIterator(this);
 	}
 }
---
+++ src/idh/java/StudentIterator.java
@@ -0,0 +1,25 @@
+package idh.java;
+
+import java.util.Iterator;
+
+public class StudentIterator implements Iterator<Student> {
+
+	Course course;
+	int currentPosition = -1; // i 
+	
+	public StudentIterator(Course course) {
+		this.course = course;
+	}
+	
+	@Override
+	public boolean hasNext() {
+		return currentPosition < course.size() - 1;
+	}
+
+	@Override
+	public Student next() {
+		currentPosition++;
+		return course.getMember(currentPosition);
+	}
+
+}
